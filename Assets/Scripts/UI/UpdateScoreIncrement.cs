2543de97f954ed9177b5f40752b16ce442ea4478
RudeManoloStudio <contact@edelbarre.net>
2025-11-23 10:06:51 +0100

    Correction bug affichage
    Conversion Text → TMP_Text dans UIManager
    Conversion des compteurs (BilleCounterText, PlombCounterText)
    Conversion du prefab UpdateScore
    Fix du script UpdateScoreIncrement.cs
    Ré-assignation du background

--- Assets/Scripts/UI/UpdateScoreIncrement.cs
+++ Assets/Scripts/UI/UpdateScoreIncrement.cs
@@ -1,15 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
+using TMPro;
 
 public class UpdateScoreIncrement : MonoBehaviour
 {
-
     private void OnEnable()
     {
-        Text text = transform.GetComponent<Text>();
-
+        TMP_Text text = transform.GetComponent<TMP_Text>();
         text.CrossFadeAlpha(0f, 1f, false);
     }
 }

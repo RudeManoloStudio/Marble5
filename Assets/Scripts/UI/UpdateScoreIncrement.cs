a811c44bb5037c16a14251b6c25218be2de043cc
RudeManoloStudio <contact@edelbarre.net>
2025-11-23 20:26:01 +0100

    Ajout score flottant

--- Assets/Scripts/UI/UpdateScoreIncrement.cs
+++ Assets/Scripts/UI/UpdateScoreIncrement.cs
@@ -1,13 +1,58 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
 public class UpdateScoreIncrement : MonoBehaviour
 {
-    private void OnEnable()
+    private TMP_Text targetScoreText;
+    private RectTransform rectTransform;
+    private TMP_Text myText;
+
+    private float flyDuration = 1.5f;
+    private float arcHeight = 150f;
+
+    private void Awake()
     {
-        TMP_Text text = transform.GetComponent<TMP_Text>();
-        text.CrossFadeAlpha(0f, 1f, false);
+        rectTransform = GetComponent<RectTransform>();
+        myText = GetComponent<TMP_Text>();
+    }
+
+    public void FlyToScore(TMP_Text scoreText)
+    {
+        targetScoreText = scoreText;
+        StartCoroutine(FlyCoroutine());
+    }
+
+    private IEnumerator FlyCoroutine()
+    {
+        Vector3 startPosition = rectTransform.position;
+        Vector3 endPosition = targetScoreText.rectTransform.position + new Vector3(50f, 0f, 0f);
+
+        float elapsed = 0f;
+
+        while (elapsed < flyDuration)
+        {
+            elapsed += Time.deltaTime;
+            float t = elapsed / flyDuration;
+
+            float smoothT = 1f - Mathf.Pow(1f - t, 3f);
+
+            Vector3 straightLine = Vector3.Lerp(startPosition, endPosition, smoothT);
+
+            float arc = Mathf.Sin(t * Mathf.PI) * arcHeight;
+
+            Vector3 curvedPosition = straightLine + Vector3.up * arc;
+
+            rectTransform.position = curvedPosition;
+
+            float scale = Mathf.Lerp(1.2f, 0.6f, smoothT);
+            rectTransform.localScale = Vector3.one * scale;
+
+            yield return null;
+        }
+
+        rectTransform.position = endPosition;
+
+        Destroy(gameObject);
     }
 }
